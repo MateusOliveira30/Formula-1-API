b7d750d1736577d4869bbedc8fcc12b6b41369f4
MateusOliveira30 <mcesaroliveira30@yahoo.com>
2024-05-28 19:37:55 -0300

    Add files via upload

---
+++ team.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { generateTeamsArray } from '../data.js';
+import { validadePosition } from '../inputValidation.js';
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).send(generateTeamsArray());
+});
+
+router.get('/standings/:position', (req, res) => {
+    const teams = generateTeamsArray();
+
+    const { position } = req.params;
+    const { error } = validadePosition(position, teams.length);
+    if(error){
+        res.status(400).send(error);
+        return;
+    }
+    const selectedTeam = teams[position - 1];
+    res.status(200).send(selectedTeam);
+});
+
+export default router;
